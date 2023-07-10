758c7d142ec701c1a37f8a275542b137fc86fea5
Matheus Augusto <matheusalgusto_@hotmail.com>
2023-07-10 14:08:45 -0300

    change build script

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.10.11-bullseye
+# FROM python:3.10.11-slim-bullseye
 
 RUN mkdir /app
 
--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -1,4 +1,4 @@
-pipenv requirements > requirements.txt
+# pipenv requirements > requirements.txt
 docker build -t terumo-service-multiple-models .
 
 docker tag terumo-service-multiple-models terumoapp/terumo-service-multiple-models:latest
